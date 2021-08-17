7bf6486fd0aaef3c16750152441af7824078c309
Nilotpal Sarkar <nilotpalsarkar@Nilotpals-MacBook-Pro.local>
2021-08-17 21:24:11 +0530

    Adding db code

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from model import History
+from mysqlDB import Database
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+database = Database()
+# myConnection = pymysql.connect(host=hostname, user=username, passwd=password, db="mysql")
+history = History("Sachin", "robin", 23)
+
+myConnection = database.connect()
+database.createDatabase(myConnection)
+database.createHistoryTable(myConnection)
+database.insert(myConnection, history)
+database.doQuery(myConnection)
+
+myConnection.close()
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    game()
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- model.py
+++ model.py
@@ -1,30 +1,8 @@
-import numpy
-import numpy as np
 
-class Player:
-    def __init__(self, player1, player2):
-        self.__player1 = player1
-        self.__player2 = player2
-        self.__score = 0
-        def show_full_name(self, player1, player2):
-            return self.player1 + ' ' + '&' + self._player2
+class History:
 
-
-    def update_score(self, score):
-        self.__score = score
-
-class TikTok_Game:
-    def __init__(self, matrix_game: numpy, game_end: bool):
-    matrix_game = np.array([3][3], dtype='int32')
-    self.game_end = game_end
-    self.matrix_game = matrix_game
-
-
-    def play_game(self):
-
-    def is_win_game(self):
-    def is_continue_game(self):
-    def is_loss_game(self):
-    def is_game_mutual(self):
-    def restart_game(self):
+    def __init__(self, player1, player2, winner):
+        self.player1 = player1
+        self.player2 = player2
+        self.winner = winner
 
